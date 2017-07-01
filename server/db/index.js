57f8604a16a82e4897b652df56bf4fe30a9f84bb
Max Hoffman <maximilian.wolfgang1@gmail.com>
2017-07-01 08:01:36 -0700

    (BUGFIX) config error

--- server/db/index.js
+++ server/db/index.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const config = require('../../config');
+const config = require('../../config.example.js');
 
 if (process.env.MODE === 'production') {
   var dbUrl = process.env.DB_URL;
